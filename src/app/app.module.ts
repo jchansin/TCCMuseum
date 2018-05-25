fd09053601ed75cf3fc97f7d40e412c8561afad9
Jerome <jchansin.tcc@gmail.com>
2018-05-24 19:51:48 -1000

    Removed unused pages (Home, Contact) Added Scan and List pages Changed icons in tabs and tab order

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
 import { TabsPage } from '../pages/tabs/tabs';
+import { ScanPage } from '../pages/scan/scan';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -15,9 +15,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    ScanPage,
+    ListPage
   ],
   imports: [
     BrowserModule,
@@ -27,9 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    ScanPage,
+    ListPage
   ],
   providers: [
     StatusBar,
